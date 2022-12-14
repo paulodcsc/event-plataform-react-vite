60475888a51dd45be37a0acce8c0ec6c5d4d33ea
Paulo Capelo <paulodcsc@gmail.com>
2022-12-14 00:23:27 -0300

    theme colors addes + first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apollo";
-
-interface iLesson {
-  id: string;
-  title: string;
-}
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
+import { Event } from "./pages/Event";
 
 function App() {
-  const { data } = useQuery<{lessons: iLesson[]}>(GET_LESSONS_QUERY);
-
-  console.log(data);
-
-  // useEffect(() => {                     //useQuery é uma alternativa menor
-  //   client
-  //     .query({
-  //       query: GET_LESSONS_QUERY,
-  //     })
-  //     .then((response) => {
-  //       console.log(response);
-  //     });
-  // }, []);
-
   return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
+    <>
+      <Event />
+    </>
   );
 }
 
